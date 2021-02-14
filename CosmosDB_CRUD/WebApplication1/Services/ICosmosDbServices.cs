126eeb6ea4c9b2b14440a9ec72097857b68c4cfa
sanketvaidya1 <79004710+sanketvaidya1@users.noreply.github.com>
2021-02-14 17:32:41 +0530

    Add files via upload

---
+++ CosmosDB_CRUD/WebApplication1/Services/ICosmosDbServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using todo.Models;
+
+    public interface ICosmosDbServices
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
